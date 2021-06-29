ab1def3c446a0981af09888ae25eaed94753bade
Phani <phanitalapaneni15@gmail.com>
2021-06-29 12:11:01 +0530

    Uploaded files from local

---
+++ jntu.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+from dotenv import load_dotenv
+import os,time
+rgno=input("Enter the full registration number to view your 3-1 cgpa and Percentage")
+option=webdriver.ChromeOptions()
+option.add_argument('headless') #Set the parameters of the option
+driver = webdriver.Chrome(chrome_options=option) # Open Google Chrome
+#driver = webdriver.Chrome("chromedriver.exe", options=opt)
+driver.get("https://jntukresults.edu.in/view-results-56736132.html")
+
+sbox = driver.find_element_by_class_name("txt")
+sbox.send_keys(rgno)
+
+submit = driver.find_element_by_class_name("ci")
+submit.click()
+time.sleep(1)
+rows = driver.find_elements_by_xpath("/html/body/div/div/div/div/center/div[1]/table/tbody/tr")
+cols = driver.find_elements_by_xpath("//*[@id='rs']/table/tbody/tr[6]/td")
+
+l=[]
+k=[]
+p=[]
+for i in rows:
+	l.append(i.text)
+for i in cols:
+	k.append(i.text)
+
+v=len(l)
+for i in range(2,v-1):
+	p.append(list(l[i].split(" "))[-2:])
+
+def grade(q):
+	if(q=="O"):
+		return 10
+	elif(q=="S"):
+		return 9
+	elif(q=="A"):
+		return 8
+	elif(q=="B"):
+		return 7
+	elif(q=="C"):
+		return 6
+	elif(q=="D"):
+		return 5
+
+def percentage(p):
+	a=0
+	for i in p:
+		a=a+grade(i[0])*int(i[1])
+	return a
+def total(p):
+	a=0
+	for i in p:
+		a=a+int(i[1])
+	return a
+cgpa = round(percentage(p)/total(p),2)
+percentage = round((percentage(p)/total(p))-0.7,2)
+print("The Cgpa is:" + str(cgpa))
+print("The Percentage is:" + str(percentage))
+
+driver.close()
+
